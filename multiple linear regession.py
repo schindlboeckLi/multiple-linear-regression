495fdcdb7df34344008fed56c711117837402bd6
schindlboeckLi <44441868+schindlboeckLi@users.noreply.github.com>
2019-07-04 17:38:22 +0200

    reorganized file

--- multiple linear regession.py
+++ multiple linear regession.py
@@ -1,61 +1,75 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 18 18:16:45 2019
+Created on Fri Jun 21 13:09:43 2019
 
 @author: lisa
 """
 
-# import relevant packages
-# pandas package is used to import data
-# statsmodels is used to inoke the functions that help in lm
-import pandas as pd
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
+# Making the imports
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
 import matplotlib.pyplot as plt
-# import dataset
-data = pd.read_csv(r'C:\Users\lisa\Documents\regAgeWeight1.txt', delimiter=',')
-X = data.iloc[:,1]
-Y = data.iloc[:, 2]
-X = [[3.54, 40],[4.29, 50],[4.59, 51],[4.79, 56],[5.24, 57],[6,62],[6.19, 63],
-    [7.04, 68],[7.19, 69],[7.5, 74],[8.59, 80]]#data.iloc[:,1]
-Y = [0,1,2,3,4,5,5,7,8,9,10]#data.iloc[:, 2]
-print(data)
+import statsmodels.api as sm
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
+#plt.rcParams['figure.figsize'] = (12.0, 9.0)
 
-#ata = data[['Age', 'Weight', 'New']].dropna()
-# run regression
+# Preprocessing Input data
+data = np.loadtxt('regAgeWeight.txt', delimiter=',')
+#print(data)
+X_1 = data[:, 0]
+X_2 = data[:, 1]
+X = data[:, 0:2]
+print(X)
+X = sm.add_constant(X)
 
-
-#X = sm.add_constant(X)
-results = smf.ols((X,Y),data=data).fit()
-#est2=est.fit()
-#print(results.summary())
-array=results.params
-#print(results.params)
-print(array)
-
-
-df2=pd.DataFrame(X,columns=['Weight','New'])
-df2['Age']=pd.Series(Y)
-print(df2)
-
-model = smf.ols(formula='Age ~ Weight + New', data=df2)
-results_formula = model.fit()
-print(results_formula.params)
-
-x_surf, y_surf = np.meshgrid(np.linspace(df2.Weight.min(), df2.Weight.max(), 110),np.linspace(df2.New.min(), df2.New.max(), 110))
-onlyX = pd.DataFrame({'Weight': x_surf.ravel(), 'New': y_surf.ravel()})
-fittedY=results_formula.predict(exog=onlyX)
-#rint(fittedY)
-z_surf=fittedY.values.reshape(x_surf.shape)
-fig= plt.figure(figsize=(18, 16))
+Y = data[:, 2]
+m = np.size(Y, 0)
+#plt.scatter(X[:,0],y[:,0])
+print(X, 'x')
+#plt.show()
+#print(X, y)
+Q = X.T.dot(X)
+Xy = X.T.dot(Y)
+Qinv = np.linalg.inv(Q)
+beta = Qinv.dot(Xy) 
+r_squared = 1 - (Y - X.dot(beta)).T.dot(Y - X.dot(beta))/ ((Y-np.mean(Y)).T.dot(Y-np.mean(Y)))
+print(beta, 'beta')
+#print()
+#print(Qinv)
+#print()
+#print(Xy)
+#print()
+#print(Q)
+   
+print(r_squared, 'R2')
+fig= plt.figure(figsize=(20, 18))
 #plt.figsize(6,15)
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(df2['Weight'],df2['New'],df2['Age'],c='black', marker='o', alpha=1)
-ax.plot_surface(x_surf,y_surf,z_surf, color='None', alpha=0.25)
-ax.set_xlabel('Weight')
-ax.set_ylabel('New')
-ax.set_zlabel('Age')
-#ax.view_init(50, -65)
-plt.show()
+a = np.linalg.solve(np.dot(X.T, X), np.dot(X.T, Y))
+predictedY = np.dot(X, a)
+print(predictedY)
+
+x_surf, y_surf = np.meshgrid(np.linspace(X_1.min(), X_1.max(), 110),np.linspace(0, X_2.max(), 110))
+Y_pred = beta[0] + beta[1]*x_surf + beta[2]*y_surf
+
+ax.scatter(X_1,X_2,Y,c='black', marker='o', alpha=1)
+ax.scatter(X_1,X_2, predictedY, color='g',alpha=0.0)
+ax.plot_surface(x_surf,y_surf,Y_pred,color='None',  alpha=0.1)
+ax.set_xlabel('Weight', fontsize= 25)
+ax.set_ylabel('Height', fontsize= 25)
+ax.set_zlabel('Age', fontsize= 25)
+ax.grid(linewidth=.1)
+#ax.view_init(45,3)
+i=0
+print('pred=', beta[0] + beta[1]*12 + beta[2]*85, )
+for xx,yy,zz in zip(X_1,X_2,predictedY):
+    res = Y[i]-(zz)
+    line=art3d.Line3D(*zip((xx, yy, zz), (xx, yy, Y[i])))
+    ax.add_line(line)
+    i+=1
+
+#plt.show()
+plt.tight_layout()
+
+plt.savefig(r'C:\urLocation',bbox_inches='tight',pad_inches=0)
